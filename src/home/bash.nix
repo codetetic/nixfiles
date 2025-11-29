0c67dc733070aee69ad1ae9e5ce83b53bf271526
Peter Measham <github@codetetic.co.uk>
2025-11-29 15:58:20 +0000

    remove extra bash config

--- src/home/bash.nix
+++
@@ -1,9 +0,0 @@
-{ ... }: {
-  programs.bash = {
-    shellAliases = {
-      nr-update = "nix flake update --flake ~/src/nixfiles";
-      nr-test = "sudo nixos-rebuild test --flake ~/src/nixfiles";
-      nr-switch = "sudo nixos-rebuild switch --flake ~/src/nixfiles";
-    };
-  };
-}
--- src/home/zsh.nix
+++ src/home/zsh.nix
@@ -11,8 +11,8 @@
 
     shellAliases = {
       nr-update = "nix flake update --flake ~/src/nixfiles";
-      nr-test = "sudo nixos-rebuild test --flake ~/src/nixfiles";
-      nr-switch = "sudo nixos-rebuild switch --flake ~/src/nixfiles";
+      nr-test = "sudo nixos-rebuild test --flake ~/src/nixfiles#$(hostname)";
+      nr-switch = "sudo nixos-rebuild switch --flake ~/src/nixfiles#$(hostname)";
     };
   };
 }
