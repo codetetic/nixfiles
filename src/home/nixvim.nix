011be3f80c7d6147def59f18efa81b076bdc60e9
Peter Measham <github@codetetic.co.uk>
2025-12-02 23:39:22 +0000

    improve neovim

--- src/home/nixvim.nix
+++ src/home/nixvim.nix
@@ -57,7 +57,25 @@
 
       settings = {
         keymap = {
-          preset = "default";
+          preset = "enter";
+        };
+
+        appearance = {
+          nerd_font_variant = "mono";
+        };
+
+        completion = {
+          documentation = {
+            auto_show = false;
+            auto_show_delay_ms = 500;
+          };
+        };
+
+        sources = {
+          default = [
+            "lsp"
+            "path"
+          ];
         };
       };
     };
